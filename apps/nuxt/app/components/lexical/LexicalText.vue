d34f7817a293e858f995b6ea529c9b2e1a44cbb6
glenn <glenn.reumers@appwise.be>
2025-05-25 11:41:55 +0200

    fix(lexical): add format property to LexicalText and update rendering logic

--- apps/nuxt/app/components/lexical/LexicalText.vue
+++ apps/nuxt/app/components/lexical/LexicalText.vue
@@ -2,15 +2,29 @@
 export interface LexicalText {
   type: 'text'
   text?: string
+  format: number
 }
 
 const props = defineProps<{
   node: LexicalText
 }>()
+
+const component = computed<string>(() => {
+  switch (props.node.format) {
+    case 1:
+      return 'strong'
+
+    case 2:
+      return 'em'
+
+    default:
+      return 'span'
+  }
+})
 </script>
 
 <template>
-  <span>
+  <component :is="component">
     {{ props.node.text }}
-  </span>
+  </component>
 </template>
--- apps/nuxt/app/features/lexical/models/lexicalTextNode.model.ts
+++ apps/nuxt/app/features/lexical/models/lexicalTextNode.model.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const lexicalTextNodeSchema = z.object({
   type: z.literal('text'),
   text: z.string().optional(),
+  format: z.number(),
 })
 
 export type LexicalTextNode = z.infer<typeof lexicalTextNodeSchema>

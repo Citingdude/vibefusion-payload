baf2377cd2ca2c9f3c1423752b5bcf465ff63598
glenn <glenn.reumers@appwise.be>
2026-05-31 12:20:58 +0200

    fix(font.plugin): remove unused variable font import

--- apps/nuxt/app/plugins/font.plugin.ts
+++ apps/nuxt/app/plugins/font.plugin.ts
@@ -4,8 +4,5 @@ import '@fontsource/alegreya-sans/500.css'
 import '@fontsource/alegreya-sans/700.css'
 import '@fontsource/alegreya-sans/800.css'
 import '@fontsource/alegreya-sans/900.css'
-import '@fontsource-variable/alegreya'
 
-export default defineNuxtPlugin(() => {
-
-})
+export default defineNuxtPlugin(() => {})
